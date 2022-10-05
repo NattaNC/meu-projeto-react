d713441f3bedc9d799e7e00fc208c7e1d364f689
Nathan Correia <naathancaue556@gmail.com>
2022-10-05 16:49:34 -0300

    Atualização React

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import { useState } from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Empresa from './pages/Empresa'
+import Contato from './pages/Contato'
+import Navbar  from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
-import './App.css';
-import SeuNome from './components/SeuNome';
-import Saudacao from './components/Saudacao';
-
-function App() {
-  const [nome, setNome] = useState()
-
+function App () {
   return (
-    <div className="App">
-      <h1>State Lift</h1>
-      <SeuNome setNome={setNome} />
-      <Saudacao nome={nome}/>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={ <Home /> } />
+        <Route path="/empresa" element={ <Empresa /> } />
+        <Route path="/contato" element={ <Contato /> } />
+      </Routes>
+      <Footer />
+    </Router>
   ) 
 }
 
 export default App
+
